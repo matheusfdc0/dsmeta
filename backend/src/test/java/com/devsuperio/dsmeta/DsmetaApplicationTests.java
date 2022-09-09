aa2274d44b949e83e6127d1657edc61558767837
matheusdv <matheusfdgit.github.io>
2022-09-09 00:06:38 -0300

    Project created

---
+++ backend/src/test/java/com/devsuperio/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperio.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
