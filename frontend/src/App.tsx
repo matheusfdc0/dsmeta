5bf676dc51a6161f2aa267b25e78b957ef905bc2
matheusdv <matheusfdgit.github.io>
2022-09-09 19:36:18 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NotificationButton from "./componentes/NotificationsButton"
+
 function App() {
   return(
-    <h1>Ola mundo!</h1>
+    <>
+      <h1>Ola mundo!</h1>
+      <NotificationButton/>
+      <NotificationButton/>
+    </>
   )
 }
 
